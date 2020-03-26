17f6d2cff55604681a6f374644ee6ca740af0a92
Jean-Karim Bockstael <jkb@jkbockstael.be>
2020-03-26 09:46:46 +0100

    Let's keep technicalities together

--- euler001-proc.py
+++ euler001-proc.py
@@ -5,15 +5,15 @@
 
 import sys
 
-def parse_input(lines):
-    return int(lines[0].strip())
-
 def euler001(bound):
     sum = 0
     for i in range(0, bound):
         if i % 3 == 0 or i % 5 == 0:
             sum += i
     return sum
+
+def parse_input(lines):
+    return int(lines[0].strip())
 
 if __name__ == "__main__":
     print(euler001(parse_input(sys.stdin.readlines())))
